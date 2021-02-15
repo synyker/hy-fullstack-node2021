8679469fd5718acb0f3ede49e7a1f87f79297676
Jonne Airaksinen <jonne.airaksinen@perfektio.fi>
2021-02-15 19:31:46 +0200

    osa3 3.10

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -110,7 +110,7 @@ app.get("/info", (req, res) => {
   res.end(html)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
